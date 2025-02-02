631eae3e69926e40040b90cea53041ecf88d7f1e
William Carroll <wpcarro@gmail.com>
2025-02-01 22:18:08 -0600

    feat: Conditionally flash wired or wirelessly
    Use wired connection when available. Otherwise try to send OTA updates.

--- Forth/flash.sh
+++ Forth/flash.sh
@@ -14,9 +14,21 @@ clang-format -i --style=file $file
 echo "[flash.sh] Compiling..."
 arduino-cli compile -b esp32:esp32:esp32 $file
 
-# NOTE: We need the empty password field. Otherwise, arduino-cli will prompt us
-# for the password.
-echo "[flash.sh] Uploading (OTA)..."
-arduino-cli upload -b esp32:esp32:esp32 $file -p 172.20.10.5 --upload-field password=""
+if [ -e /dev/cu.usbserial-0001 ]; then
+  echo "[flash.sh] Detected wired connection."
+
+  echo "[flash.sh] Killing existing serial connections..."
+  pkill picocom || echo "[flash.sh] Could not find any"
+
+  echo "[flash.sh] Uploading..."
+  arduino-cli upload -b esp32:esp32:esp32 $file -p /dev/cu.usbserial-0001
+else
+  echo "[flash.sh] No wired connection found. Attempting OTA update instead..."
+
+  # NOTE: We need the empty password field. Otherwise, arduino-cli will prompt us
+  # for the password.
+  echo "[flash.sh] Uploading (OTA)..."
+  arduino-cli upload -b esp32:esp32:esp32 $file -p 172.20.10.5 --upload-field password=""
+fi
 
 echo "[flash.sh] Firmware updated!"
